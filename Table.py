fada4ca3354e33214626c4ebad03866147a44c83
Bravoth0729v <91524372+Bravoth0729v@users.noreply.github.com>
2021-12-11 10:03:41 +0530

    Souvik

---
+++ Table.py
@@ -0,0 +1,7 @@
+username = input("Enter Username: ")
+print("Your Username is: ",username)
+a = int(input("Enter Any Number: "))
+for n in range(1,int(input("Enter Any No: "))):
+    print(a,'x',n, '=', a*n)
+
+print('Thank You For Using This Product.')
